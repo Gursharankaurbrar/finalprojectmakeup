a4d325ae6bb789ef42bf3e43aad4fd1efe13c741
RRC-Gursharan <143541652+Gursharankaurbrar@users.noreply.github.com>
2025-04-23 10:16:43 -0500

    Documented MakeupDao file

--- app/src/main/java/com/example/finalprojectmakeup/api/db/MakeupDao.kt
+++ app/src/main/java/com/example/finalprojectmakeup/api/db/MakeupDao.kt
@@ -6,15 +6,28 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.finalprojectmakeup.api.model.MakeupDataItem
 
-
+/**
+ * Data Access Object (DAO) for accessing makeup data in the local Room database.
+ * Provides methods for inserting and querying makeup products.
+ */
 @Dao
 interface MakeupDao {
+    /**
+     * Inserts a list of makeup items into the database.
+     * If a conflict occurs (e.g., duplicate primary key), the existing record will be replaced.
+     **/
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll( makeups: List<MakeupDataItem>)
 
+    /**
+     * Retrieves a makeup item from the database based on its unique ID.
+     **/
     @Query("SELECT * FROM makeups WHERE id = :id")
     fun getMakeupById(id: Int) : MakeupDataItem?
 
+    /**
+     * Retrieves all makeup items from the database.
+     **/
     @Query("SELECT * FROM makeups")
     fun getAllMakeup(): List<MakeupDataItem>
 
